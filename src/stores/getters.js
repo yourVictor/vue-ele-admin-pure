20244f36ee909e41e9cb508e4b25aba6f3c633d5
Mole <wangzhiming@bisinuolan.com>
2019-04-18 16:01:26 +0800

    delete other file

--- src/stores/getters.js
+++
@@ -1,7 +0,0 @@
-export default {
-  token: state => state.account.token,
-  account: state => state.account.account,
-  routes: state => state.account.routes,
-  sidebar: state => state.app.sidebar,
-  visitedTagViews: state => state.tagView.visitedTagViews
-}
