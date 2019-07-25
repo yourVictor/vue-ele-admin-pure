dc2351c46172a65782a82dc4ac2574bcfbeb9ab1
Mole <wangzhiming@bisinuolan.com>
2019-07-25 18:01:04 +0800

    add support to keep-alive

---
+++ precache-manifest.e4f917a5aa765e65494399f63284a4e6.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "2183083294593355e617",
+    "url": "/vue-ele-admin-pure/static/css/member.76f17c66.css"
+  },
+  {
+    "revision": "d88d1a319698b94521a3",
+    "url": "/vue-ele-admin-pure/static/css/account.d71c9e7d.css"
+  },
+  {
+    "revision": "172607631c554f6461e8",
+    "url": "/vue-ele-admin-pure/static/css/app.5a41df93.css"
+  },
+  {
+    "revision": "172607631c554f6461e8",
+    "url": "/vue-ele-admin-pure/static/js/app.21ff11cc.js"
+  },
+  {
+    "revision": "1e7b1ce9e3b2c4a9aa23",
+    "url": "/vue-ele-admin-pure/static/js/chunk-54ec5630.446e6ec9.js"
+  },
+  {
+    "revision": "e8255bb39621a029d2ce",
+    "url": "/vue-ele-admin-pure/static/css/chunk-vendors.c8f9f004.css"
+  },
+  {
+    "revision": "e8255bb39621a029d2ce",
+    "url": "/vue-ele-admin-pure/static/js/chunk-vendors.c93cd7cb.js"
+  },
+  {
+    "revision": "078eb42223e05a03c419",
+    "url": "/vue-ele-admin-pure/static/css/components.b1028904.css"
+  },
+  {
+    "revision": "078eb42223e05a03c419",
+    "url": "/vue-ele-admin-pure/static/js/components.5248be8d.js"
+  },
+  {
+    "revision": "7a085ca6ab9cfa54cd43",
+    "url": "/vue-ele-admin-pure/static/css/dashboard.d5ed23b6.css"
+  },
+  {
+    "revision": "7a085ca6ab9cfa54cd43",
+    "url": "/vue-ele-admin-pure/static/js/dashboard.8acc45e7.js"
+  },
+  {
+    "revision": "ccb710f33ac9e91fad99",
+    "url": "/vue-ele-admin-pure/static/css/error.788d88f7.css"
+  },
+  {
+    "revision": "ccb710f33ac9e91fad99",
+    "url": "/vue-ele-admin-pure/static/js/error.ab915f41.js"
+  },
+  {
+    "revision": "d88d1a319698b94521a3",
+    "url": "/vue-ele-admin-pure/static/js/account.409df8b8.js"
+  },
+  {
+    "revision": "2183083294593355e617",
+    "url": "/vue-ele-admin-pure/static/js/member.c63d6699.js"
+  },
+  {
+    "revision": "f034d3ac98652bdb5068",
+    "url": "/vue-ele-admin-pure/static/css/setting.ccb4c422.css"
+  },
+  {
+    "revision": "f034d3ac98652bdb5068",
+    "url": "/vue-ele-admin-pure/static/js/setting.16add64d.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/vue-ele-admin-pure/static/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/vue-ele-admin-pure/static/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "68e233267ea06fdfa5499befdb808206",
+    "url": "/vue-ele-admin-pure/static/img/page_normal.68e23326.jpg"
+  },
+  {
+    "revision": "555680c125ba6318618e3db5a39b1672",
+    "url": "/vue-ele-admin-pure/static/img/page_404.555680c1.gif"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-ele-admin-pure/static/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-ele-admin-pure/robots.txt"
+  },
+  {
+    "revision": "a1c7e30044c4a5852f82fb1c1a973a96",
+    "url": "/vue-ele-admin-pure/index.html"
+  },
+  {
+    "revision": "0f515dea1eebb530f66b890d3dbc9e96",
+    "url": "/vue-ele-admin-pure/img/donate/wechat_donate.jpg"
+  },
+  {
+    "revision": "d7bcd740d3349ff35122d5c3b45487b7",
+    "url": "/vue-ele-admin-pure/img/bg/page_login.jpg"
+  },
+  {
+    "revision": "c35a54894421397ad878036f33d8d4bd",
+    "url": "/vue-ele-admin-pure/WEB-INF/web.xml"
+  }
+];
