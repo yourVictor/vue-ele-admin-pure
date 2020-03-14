0da277b27baace326d06f22efdb51dea1e4aae6f
mole <731913218@qq.com>
2020-03-14 21:11:17 +0800

    修改打包配置

---
+++ initConfigFile.js
@@ -0,0 +1,31 @@
+var fs = require('fs')
+var path = require('path')
+
+var project = 'test'
+try {
+  project = process.argv.splice(3)[0]
+} catch (e) {
+  console.log(e)
+}
+var fileList = [
+  {
+    srcPath: path.join(__dirname, `src/projects/${project}/.env`),
+    destPath: path.join(__dirname, '.env')
+  },
+  {
+    srcPath: path.join(__dirname, `src/projects/${project}/.env.test`),
+    destPath: path.join(__dirname, '.env.test')
+  },
+  {
+    srcPath: path.join(__dirname, `src/projects/${project}/.env.uat`),
+    destPath: path.join(__dirname, '.env.uat')
+  },
+  {
+    srcPath: path.join(__dirname, `src/projects/${project}/.env.prod`),
+    destPath: path.join(__dirname, '.env.prod')
+  }
+]
+console.log(`project: ${project}`)
+for (let i = 0; i < fileList.length; i++) {
+  fs.copyFileSync(fileList[i].srcPath, fileList[i].destPath)
+}
