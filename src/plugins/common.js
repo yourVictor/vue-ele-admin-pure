671b84a61a871b0531b0bf4932bfd6d131f57866
mole <731913218@qq.com>
2020-05-16 17:12:18 +0800

    修改退出登录重新刷新

--- src/plugins/common.js
+++ src/plugins/common.js
@@ -32,7 +32,7 @@ common.install = function (Vue) {
       // 逻辑...
     },
     updated: debounce(function () {
-      if (this.$route.name === 'login') {
+      if (this.$route && this.$route.name === 'login') {
         return
       }
       // 长时间未操作自动退出登录
