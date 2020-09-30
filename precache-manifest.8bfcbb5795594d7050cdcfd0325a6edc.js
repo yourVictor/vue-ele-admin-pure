cfd8cc3e3b1e7a3a145ba7e87a6d0f0416891664
macbook air <macbookair@yidian-deMacBook-Air.local>
2020-09-30 17:12:04 +0800

    新增组件

---
+++ precache-manifest.8bfcbb5795594d7050cdcfd0325a6edc.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296a81d99a3ffc015f979f7899b91602",
+    "url": "/vue-ele-admin-pure/WEB-INF/web.xml"
+  },
+  {
+    "revision": "d7bcd740d3349ff35122d5c3b45487b7",
+    "url": "/vue-ele-admin-pure/img/bg/page_login.jpg"
+  },
+  {
+    "revision": "0f515dea1eebb530f66b890d3dbc9e96",
+    "url": "/vue-ele-admin-pure/img/donate/wechat_donate.jpg"
+  },
+  {
+    "revision": "94df824d6ba89859844fcc8fe8e4693f",
+    "url": "/vue-ele-admin-pure/index.html"
+  },
+  {
+    "revision": "7edbb8a503a6e746ea2bcb398e39d45f",
+    "url": "/vue-ele-admin-pure/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-ele-admin-pure/robots.txt"
+  },
+  {
+    "revision": "3deb11170cfff6081ae0",
+    "url": "/vue-ele-admin-pure/static/css/account.820aadd7.css"
+  },
+  {
+    "revision": "40e979730b5c776ee54e",
+    "url": "/vue-ele-admin-pure/static/css/app.22e19e67.css"
+  },
+  {
+    "revision": "253645bbe84b9c8c10e0",
+    "url": "/vue-ele-admin-pure/static/css/chunk-vendors.c957625a.css"
+  },
+  {
+    "revision": "4afc8b3ab4c7a77c52cb",
+    "url": "/vue-ele-admin-pure/static/css/components.fdc429b9.css"
+  },
+  {
+    "revision": "8da91ed284af005f35b6",
+    "url": "/vue-ele-admin-pure/static/css/dashboard.f5f3dc8b.css"
+  },
+  {
+    "revision": "a350a316f8e1432731ef",
+    "url": "/vue-ele-admin-pure/static/css/error.b634b0c3.css"
+  },
+  {
+    "revision": "cb68bedfa9714f11c4d0",
+    "url": "/vue-ele-admin-pure/static/css/member.7d964c15.css"
+  },
+  {
+    "revision": "7d2bfda4855f185c84d7",
+    "url": "/vue-ele-admin-pure/static/css/setting.3855ce14.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/vue-ele-admin-pure/static/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/vue-ele-admin-pure/static/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-ele-admin-pure/static/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "555680c125ba6318618e3db5a39b1672",
+    "url": "/vue-ele-admin-pure/static/img/page_404.555680c1.gif"
+  },
+  {
+    "revision": "68e233267ea06fdfa5499befdb808206",
+    "url": "/vue-ele-admin-pure/static/img/page_normal.68e23326.jpg"
+  },
+  {
+    "revision": "3deb11170cfff6081ae0",
+    "url": "/vue-ele-admin-pure/static/js/account.5b961212.js"
+  },
+  {
+    "revision": "40e979730b5c776ee54e",
+    "url": "/vue-ele-admin-pure/static/js/app.adff4380.js"
+  },
+  {
+    "revision": "253645bbe84b9c8c10e0",
+    "url": "/vue-ele-admin-pure/static/js/chunk-vendors.c0974f72.js"
+  },
+  {
+    "revision": "4afc8b3ab4c7a77c52cb",
+    "url": "/vue-ele-admin-pure/static/js/components.4b251d58.js"
+  },
+  {
+    "revision": "8da91ed284af005f35b6",
+    "url": "/vue-ele-admin-pure/static/js/dashboard.57f09ccb.js"
+  },
+  {
+    "revision": "a350a316f8e1432731ef",
+    "url": "/vue-ele-admin-pure/static/js/error.62a1506c.js"
+  },
+  {
+    "revision": "cb68bedfa9714f11c4d0",
+    "url": "/vue-ele-admin-pure/static/js/member.cfc2a79d.js"
+  },
+  {
+    "revision": "7d2bfda4855f185c84d7",
+    "url": "/vue-ele-admin-pure/static/js/setting.79c4f7e0.js"
+  },
+  {
+    "revision": "839c094690e3decfcd835764f5adb540",
+    "url": "/vue-ele-admin-pure/static/tinymce/langs/zh_CN.js"
+  },
+  {
+    "revision": "abdd83f838fac3428c028294f27efc4e",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/content/dark/content.css"
+  },
+  {
+    "revision": "4c0b8cf274d116c2ff8b3f1c7cfaf9d3",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/content/dark/content.min.css"
+  },
+  {
+    "revision": "db31e42bcf1c9c26eb3c9281816a3b33",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/content/default/content.css"
+  },
+  {
+    "revision": "5022f9908e1f0680cdc3ad293816bd13",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/content/default/content.min.css"
+  },
+  {
+    "revision": "24d66dddf2faa9f48de40df30ae8f1fc",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/content/document/content.css"
+  },
+  {
+    "revision": "0ccaf40378ed037f42d01279f625793d",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/content/document/content.min.css"
+  },
+  {
+    "revision": "3cd6fb97c8178ed816ce946d8b601ffa",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/content/writer/content.css"
+  },
+  {
+    "revision": "856c1120d71e64f227e546a9406587f6",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/content/writer/content.min.css"
+  },
+  {
+    "revision": "dcd8ef31648a56d5f5b6a91bbd5e6d8f",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide-dark/content.css"
+  },
+  {
+    "revision": "bbe0a0560d23053f18c49fd29c49f8cb",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide-dark/content.inline.css"
+  },
+  {
+    "revision": "a88341602c874be86306fb625949da77",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide-dark/content.inline.min.css"
+  },
+  {
+    "revision": "6e64ea2a1142022ae117506732f7dae6",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide-dark/content.min.css"
+  },
+  {
+    "revision": "e84062c0d71bace6637586be7c2590d3",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide-dark/content.mobile.css"
+  },
+  {
+    "revision": "411c2608b6be78849a76c0ed14200234",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide-dark/content.mobile.min.css"
+  },
+  {
+    "revision": "baecf466c40e709e7ffdbc935fc0813a",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide-dark/fonts/tinymce-mobile.woff"
+  },
+  {
+    "revision": "7f59e38576b88cb0461893ba0b820834",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide-dark/skin.css"
+  },
+  {
+    "revision": "716b1214567d1a2fb582f77f07f28d63",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide-dark/skin.min.css"
+  },
+  {
+    "revision": "338b06826c0e8a681688fc195dbb72b1",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide-dark/skin.mobile.css"
+  },
+  {
+    "revision": "4fdf33191102d7a24a5bf0639040d128",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide-dark/skin.mobile.min.css"
+  },
+  {
+    "revision": "65e0a531e87545f66a771a6c82481d45",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide/content.css"
+  },
+  {
+    "revision": "bbe0a0560d23053f18c49fd29c49f8cb",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide/content.inline.css"
+  },
+  {
+    "revision": "a88341602c874be86306fb625949da77",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide/content.inline.min.css"
+  },
+  {
+    "revision": "715d7febbc5b2ae1df5ff1e9b666af48",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide/content.min.css"
+  },
+  {
+    "revision": "e84062c0d71bace6637586be7c2590d3",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide/content.mobile.css"
+  },
+  {
+    "revision": "411c2608b6be78849a76c0ed14200234",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide/content.mobile.min.css"
+  },
+  {
+    "revision": "baecf466c40e709e7ffdbc935fc0813a",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide/fonts/tinymce-mobile.woff"
+  },
+  {
+    "revision": "8c49db52caf20a8b94cd1f374b2e8325",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide/skin.css"
+  },
+  {
+    "revision": "2c574babb0b2e33435ce76bb219915b2",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide/skin.min.css"
+  },
+  {
+    "revision": "338b06826c0e8a681688fc195dbb72b1",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide/skin.mobile.css"
+  },
+  {
+    "revision": "4fdf33191102d7a24a5bf0639040d128",
+    "url": "/vue-ele-admin-pure/static/tinymce/skins/ui/oxide/skin.mobile.min.css"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/vue-ele-admin-pure/precache-manifest.8bfcbb5795594d7050cdcfd0325a6edc.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "vue-ele-admin"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
